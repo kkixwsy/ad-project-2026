4f5a991918b0f6499ea7c6a9a3f528571f168097
Diana <erediana88@gmail.com>
2021-01-01 22:13:20 +0300

    5.5

--- src/App.vue
+++ src/App.vue
@@ -26,6 +26,11 @@ v-for="link in links"
 </v-navigation-drawer>
 <v-app-bar app dark color="primary">
 <v-app-bar-nav-icon @click="drawer = !drawer"></v-	app-bar-nav-icon>
+<v-toolbar-title>
+<v-btn to="/">
+Home
+</v-btn>
+</v-toolbar-title>
 <v-spacer></v-spacer>
 <v-toolbar-items class="hidden-sm-and-down">
 <v-btn
