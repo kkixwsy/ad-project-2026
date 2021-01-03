2f4a23d932a206bd5f123040cfac16e77cb74801
Diana <erediana88@gmail.com>
2021-01-03 12:52:15 +0300

    3.1

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,68 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+<v-app>
+<v-navigation-drawer app v-model="drawer">
+<v-list-item>
+<v-list-item-content>
+<v-list-item-title class="title">
+КИПУ
+</v-list-item-title>
+<v-list-item-subtitle>
+Учебный проект
+</v-list-item-subtitle>
+</v-list-item-content>
+</v-list-item>
+<v-divider></v-divider>
+<v-list dense>
+<v-list-item
+v-for="link in links"
+:key="link.title"
+>
+<template v-slot:prepend>
+<v-icon :icon="link.icon"></v-icon>
+</template>
+<v-list-item-title>{{ link.title }}</v-list-	item-title>
+</v-list-item>
+</v-list>
+</v-navigation-drawer>
+<v-app-bar app dark color="primary">
+<v-app-bar-nav-icon @click="drawer = !drawer"></v-	app-bar-nav-icon>
+<v-spacer></v-spacer>
+<v-toolbar-items class="hidden-sm-and-down">
+<v-btn
+text
+v-for="link in links"
+:key="link.title"
+>
+<v-icon
+start
+:icon="link.icon"
+></v-icon>
+{{ link.title }}
+</v-btn>
+</v-toolbar-items>
+</v-app-bar>
+<v-main>
+<router-view></router-view>
+</v-main>
+</v-app>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-nav {
-  padding: 30px;
-}
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
 
-nav a.router-link-exact-active {
-  color: #42b983;
+<script>
+export default {
+data() {
+return {
+drawer: false,
+links: [
+{title:"Login", icon:"mdi-lock", url:"/login"},
+{title:"Registration",icon:"mdi-face",url:"/registration"},
+{title:"Orders",icon:"mdi-bookmark-multiple-outline", url:"/orders"},
+{title:"New ad", icon:"mdi-note-plus-outline", url:"/new"},
+{title:"My ads", icon:"mdi-view-list-outline", url:"/list"}
+]
 }
-</style>
+}
+}
+</script>
