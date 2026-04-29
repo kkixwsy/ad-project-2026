24ae41b1556725d760751418dba4e61ed767c19e
Diana <erediana88@gmail.com>
2026-04-29 22:48:34 +0300

    14

--- src/store/user.js
+++ src/store/user.js
@@ -41,6 +41,32 @@ export default {
           throw new Error('Упс... Ошибка регистрации')
         })
       }
+    },
+    
+    async loginUser({ commit }, { email, password }) {
+      commit('clearError')
+      commit('setLoading', true)
+      
+      // Здесь выполняется запрос на сервер
+      let isRequestOk = true  // Для успешного входа
+      // let isRequestOk = false  // Для имитации ошибки
+      
+      let promise = new Promise(function(resolve) {
+        setTimeout(() => resolve('Done'), 3000)
+      })
+      
+      if (isRequestOk) {
+        await promise.then(() => {
+          commit('setUser', new User(1, email, password))
+          commit('setLoading', false)
+        })
+      } else {
+        await promise.then(() => {
+          commit('setLoading', false)
+          commit('setError', 'Ошибка логина или пароля')
+          throw new Error('Упс... Ошибка логина или пароля')
+        })
+      }
     }
   },
   getters: {
