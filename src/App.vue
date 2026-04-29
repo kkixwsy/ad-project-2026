112877754cf1d925bed9a94d540ace9e9fbe7eb6
Diana <erediana88@gmail.com>
2026-04-29 23:00:40 +0300

    15.2

--- src/App.vue
+++ src/App.vue
@@ -77,19 +77,29 @@
 export default {
   data() {
     return {
-      drawer: false,
-      links: [
-        { title: "Login", icon: "mdi-lock", url: "/login" },
-        { title: "Registration", icon: "mdi-face", url: "/registration" },
-        { title: "Orders", icon: "mdi-bookmark-multiple-outline", url: "/orders" },
-        { title: "New ad", icon: "mdi-note-plus-outline", url: "/new" },
-        { title: "My ads", icon: "mdi-view-list-outline", url: "/list" }
-      ]
+      drawer: false
     }
   },
   computed: {
+    isUserLoggedIn() {
+      return this.$store.getters.isUserLoggedIn
+    },
     error() {
       return this.$store.getters.error
+    },
+    links() {
+      if (this.isUserLoggedIn) {
+        return [
+          { title: "Orders", icon: "mdi-bookmark-multiple-outline", url: "/orders" },
+          { title: "New ad", icon: "mdi-note-plus-outline", url: "/new" },
+          { title: "My ads", icon: "mdi-view-list-outline", url: "/list" }
+        ]
+      } else {
+        return [
+          { title: "Login", icon: "mdi-lock", url: "/login" },
+          { title: "Registration", icon: "mdi-face", url: "/registration" }
+        ]
+      }
     }
   },
   methods: {
