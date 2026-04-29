cf2be5b01cb3284bf553d418cc7d45ced97ae25c
Diana <erediana88@gmail.com>
2026-04-29 23:28:08 +0300

    17.5

--- src/store/ads.js
+++ src/store/ads.js
@@ -38,6 +38,16 @@ export default {
   mutations: {
     createAd(state, payload) {
       state.ads.push(payload)
+    },
+    loadAds(state, payload) {
+      state.ads = payload
+    },
+    updateAd(state, { title, desc, id }) {
+      const ad = state.ads.find(a => {
+        return a.id === id
+      })
+      ad.title = title
+      ad.desc = desc
     }
   },
   actions: {
@@ -65,6 +75,32 @@ export default {
           commit('setLoading', false)
           commit('setError', 'Ошибка создания объявления')
           throw new Error('Упс... Ошибка создания объявления')
+        })
+      }
+    },
+    
+    async updateAd({ commit }, { title, desc, id }) {
+      commit('clearError')
+      commit('setLoading', true)
+      
+      // Заглушка запроса
+      let isRequestOk = true
+      // let isRequestOk = false  // Для имитации ошибки
+      
+      let promise = new Promise(function(resolve) {
+        setTimeout(() => resolve('Done'), 3000)
+      })
+      
+      if (isRequestOk) {
+        await promise.then(() => {
+          commit('updateAd', { title, desc, id })
+          commit('setLoading', false)
+        })
+      } else {
+        await promise.then(() => {
+          commit('setLoading', false)
+          commit('setError', 'Ошибка редактирования объявления')
+          throw new Error('Упс... Ошибка редактирования объявления')
         })
       }
     }
